1462b2ab104c468e7a46f4dfffa149865f938178
Yuto Yamakita <yuvto.0420@gmail.com>
2022-10-13 07:42:36 -0700

    make it better

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
 const app = express();
-// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const mongooseUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3juubee.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 app.use(bodyParser.json());
@@ -54,6 +54,8 @@ mongoose
         console.log('Mongo is connected')
     })
     .then(() =>{
-        app.listen(5000);
+        app.listen(PORT, () =>{
+            console.log(`Server is running on ${PORT}`)
+        });
     })
     .catch(err => console.log('error message', err.message));
